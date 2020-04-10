4bd9345c3ebf94b148e5210a79363113ce9fa8bc
Ridho Febriansa <officialridhof@gmail.com>
2020-04-10 13:14:51 +0700

    remove POST from routes, fix redirect on error page, add dynamic year on footer base

--- app/mod_landing_page/controllers.py
+++ app/mod_landing_page/controllers.py
@@ -5,14 +5,14 @@ from flask import Blueprint, render_template, send_from_directory
 
 MOD_LANDING_PAGE = Blueprint('landing_page', __name__, url_prefix='/')
 
-@MOD_LANDING_PAGE.route('', methods=['GET', 'POST'])
+@MOD_LANDING_PAGE.route('', methods=['GET'])
 def landing_page():
     """
     Return a HTML of Landing Page.
     """
     return render_template("test.html")
 
-@MOD_LANDING_PAGE.route('/robots.txt', methods=['GET', 'POST'])
+@MOD_LANDING_PAGE.route('/robots.txt', methods=['GET'])
 def robots():
     """
     Return robot.txt file.
