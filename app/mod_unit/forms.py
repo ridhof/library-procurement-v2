e57336f67f18f419ce8b3b66c7972438f24d3704
Ridho Febriansa <officialridhof@gmail.com>
2020-04-11 17:41:19 +0700

    add unit form

---
+++ app/mod_unit/forms.py
@@ -0,0 +1,19 @@
+"""
+Unit Module's Forms
+"""
+from flask_wtf import FlaskForm
+from wtforms import TextField
+
+from wtforms.validators import Required
+
+
+class UnitForm(FlaskForm):
+    """
+    Forms to Create/Update Unit
+    """
+    kode_fakultas = TextField('KodeFakultas', [Required()])
+    kode_jurusan = TextField('KodeJurusan', [Required()])
+    nama_unit = TextField('NamaUnit', [Required()])
+    gedung = TextField('NamaGedung', [Required()])
+    lantai = TextField('Lantai', [Required()])
+    ruangan = TextField('Ruangan', [Required()])
