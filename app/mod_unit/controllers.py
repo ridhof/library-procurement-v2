f0b126f63813b8317a7129eed52409443d6abb74
Ridho Febriansa <officialridhof@gmail.com>
2020-04-13 17:42:13 +0700

    query read using is_delete, on insert check if kode already used, add update and delete routes

--- app/mod_unit/controllers.py
+++ app/mod_unit/controllers.py
@@ -10,7 +10,7 @@ from app.mod_unit.models import Unit
 
 from common import code, flash_code
 
-MOD_UNIT = Blueprint('unit', __name__, url_prefix='/unit')
+MOD_UNIT = Blueprint('unit', __name__, url_prefix='/unit/')
 
 @MOD_UNIT.route('', methods=['GET'])
 def table():
@@ -19,7 +19,7 @@ def table():
     """
     if Staff.is_login() is None:
         return redirect(url_for('auth.login'))
-    units = Unit.query.all()
+    units = Unit.get_all()
     return render_template("unit/table.html", units=units)
 
 @MOD_UNIT.route('/baru', methods=['GET', 'POST'])
@@ -39,11 +39,79 @@ def create():
             form.lantai.data,
             form.ruangan.data
         )
-        error = unit.insert()
-        if error:
-            flash("Terjadi kesalahan", flash_code.DANGER)
+        if Unit.query.filter_by(kode=unit.kode, is_delete=0).first is not None:
+            flash(f"Gagal menambahkan data, Unit dengan kode { unit.kode } sudah digunakan", flash_code.DANGER)
         else:
-            flash("Data berhasil disimpan", flash_code.SUCCESS)
-            return redirect(url_for('unit.create'))
-    form = UnitForm()
-    return render_template("unit/form.html", form=form)
+            if unit.insert():
+                flash(f"Unit dengan kode { unit.kode } berhasil disimpan", flash_code.SUCCESS)
+                return redirect(url_for('unit.create'))
+            else:
+                flash("Terjadi kesalahan", flash_code.DANGER)
+    return render_template("unit/form.html", form=form, page_title="Buat Unit Baru")
+
+@MOD_UNIT.route('/<unit_id>/<unit_kode>/ubah', methods=['GET', 'POST'])
+def update(unit_id, unit_kode):
+    """
+    Return Update Page
+    """
+    if Staff.is_login() is None:
+        return redirect(url_for('auth.login'))
+
+    unit = Unit.get_by_kode(unit_id, unit_kode)
+    if unit is None:
+        flash(f"Terjadi kesalahan, Unit dengan kode { unit_kode } tidak dapat ditemukan.", flash_code.WARNING)
+        return redirect(url_for('unit.table'))
+    form = UnitForm(request.form)
+    if form.validate_on_submit():
+        new_kode = f"{form.kode_fakultas.data}{form.kode_jurusan.data}"
+
+        if Unit.query.filter_by(kode=new_kode, is_delete=0).first() is None:
+            unit_update = Unit.update(
+                unit_id=form.unit_id.data, 
+                kode_unit=new_kode,
+                nama=form.nama_unit.data,
+                gedung=form.gedung.data,
+                lantai=form.lantai.data,
+                ruangan=form.ruangan.data
+            )
+            if unit_update:
+                flash(f"Unit dengan kode { unit_kode } telah berhasil diubah", flash_code.SUCCESS)
+                return redirect(url_for("unit.update", unit_id=unit_id, unit_kode=new_kode))
+        else:
+            flash(f"Unit dengan kode { new_kode } sudah digunakan", flash_code.WARNING)
+        
+        flash(f"Terjadi kesalahan sehingga data gagal diubah", flash_code.DANGER)
+        form = UnitForm(
+            data={
+                'unit_id': form.unit_id.data,
+                'kode_fakultas': form.kode_fakultas.data,
+                'kode_jurusan': form.kode_jurusan.data,
+                'nama_unit': form.nama_unit.data,
+                'gedung': form.gedung.data,
+                'lantai': form.lantai.data,
+                'ruangan': form.ruangan.data
+            }
+        )
+    else:
+        form = UnitForm(
+            data={
+                'unit_id': unit_id,
+                'kode_fakultas': unit.kode[0],
+                'kode_jurusan': unit.kode[1],
+                'nama_unit': unit.nama,
+                'gedung': unit.gedung,
+                'lantai': unit.lantai,
+                'ruangan': unit.ruangan
+            }
+        )
+    return render_template("unit/form.html", form=form, page_title="Ubah Unit")
+
+@MOD_UNIT.route('/<unit_id>/<kode_unit>hapus', methods=['GET'])
+def delete(unit_id, kode_unit):
+    unit_delete = Unit.delete(unit_id)
+    if unit_delete:
+        flash(f"Unit dengan kode { kode_unit } telah berhasil dihapus", flash_code.SUCCESS)
+        return redirect(url_for("unit.table"))
+    else:
+        flash(f"Terjadi kesalahan, unit dengan kode { kode_unit } gagal dihapus", flash_code.DANGER)
+        return redirect(url_for("unit.update", unit_id=unit_id, unit_kode=kode_unit))
