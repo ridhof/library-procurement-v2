00ed0dc2493505a55090b4493dbe27a1463f9516
Ridho Febriansa <officialridhof@gmail.com>
2020-04-15 16:57:59 +0700

    update routes

--- app/mod_staff/controllers.py
+++ app/mod_staff/controllers.py
@@ -67,3 +67,73 @@ def create():
             else:
                 flash("Terjadi kesalahan, gagal menyimpan data", flash_code.DANGER)
     return render_template("staff/form.html", form=form, page_title="Tambah Staff Baru", user=user)
+
+@MOD_STAFF.route('/<staff_id>/<staff_npk>/ubah', methods=['GET', 'POST'])
+def update(staff_id, staff_npk):
+    """
+    Return Update Page
+    """
+    user = Staff.is_login()
+    if user is None:
+        return redirect(url_for('auth.login'))
+
+    staff = Staff.get_by_npk(staff_id, staff_npk)
+    if staff is None:
+        flash(f"Terjadi kesalahan, Staff dengan kode { staff_npk } tidak dapat ditemukan", flash_code.WARNING)
+        return redirect(url_for('staff.table'))
+
+    form = StaffForm(request.form)
+    if form.validate_on_submit():
+        if staff.npk != form.npk.data:
+            if Staff.query.filter_by(npk=form.npk.data, is_delete=0).first() is not None:
+                flash(f"Terjadi kesalahan, Staff dengan NPK { form.npk.data } sudah digunakan", flash_code.WARNING)
+                form = StaffForm(
+                    data={
+                        'staff_id': form.staff_id.data,
+                        'npk': form.npk.data,
+                        'nama': form.nama.data,
+                        'role': form.role.data
+                    }
+                )
+                return render_template(
+                    "staff/form.html", 
+                    form=form, 
+                    page_title="Ubah Staff", 
+                    user=user
+                )
+
+
+        staff_update = Staff.update(
+            staff_id=staff_id,
+            npk=form.npk.data,
+            nama=form.nama.data,
+            role=form.role.data
+        )
+        if staff_update:
+            flash(f"Staff dengan NPK { staff_npk } telah berhasil diubah", flash_code.SUCCESS)
+            return redirect(url_for("staff.update", staff_id=staff_id, staff_npk=form.npk.data))
+
+        flash(f"Terjadi kesalahan pada proses perubahan, data gagal disimpan", flash_code.DANGER)
+        form = StaffForm(
+            data={
+                'staff_id': form.staff_id.data,
+                'npk': form.npk.data,
+                'nama': form.nama.data,
+                'role': form.role.data
+            }
+        )
+    else:
+        form = StaffForm(
+            data={
+                'staff_id': staff_id,
+                'npk': staff.npk,
+                'nama': staff.nama,
+                'role': staff.get_unit_role()
+            }
+        )
+    return render_template(
+        "staff/form.html", 
+        form=form, 
+        page_title="Ubah Staff", 
+        user=user
+    )
