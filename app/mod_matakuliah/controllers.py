7d9e5c87e8df2db8d70d7559561e6647e5a08d91
Ridho Febriansa <officialridhof@gmail.com>
2020-04-20 16:46:00 +0700

    add Insert Matakuliah controllers, forms, and views

--- app/mod_matakuliah/controllers.py
+++ app/mod_matakuliah/controllers.py
@@ -1,10 +1,13 @@
 """
 Matakuliah Module's Controllers
 """
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 
 from app.mod_auth.models import Staff
+from app.mod_matakuliah.forms import MatakuliahForm
 from app.mod_matakuliah.models import Matakuliah
+
+from common import flash_code
 
 MOD_MATAKULIAH = Blueprint('matakuliah', __name__, url_prefix='/matakuliah/')
 
@@ -20,3 +23,36 @@ def table():
 
     matakuliahs = Matakuliah.get_by_unit(user.unit_id)
     return render_template("matakuliah/table.html", matakuliahs=matakuliahs, user=user)
+
+@MOD_MATAKULIAH.route('/baru', methods=['GET', 'POST'])
+def create():
+    """
+    Return Create Page
+    """
+    user = Staff.is_login()
+    if user is None:
+        return redirect(url_for('auth.login'))
+
+    form = MatakuliahForm(request.form)
+    if form.validate_on_submit():
+
+        matakuliah = Matakuliah(
+            kode=form.kode.data,
+            nama=form.nama.data,
+            sks=form.sks.data,
+            deskripsi_singkat=form.deskripsi_singkat.data,
+            standar_kompetensi=form.standar_kompetensi.data,
+            unit_id=user.unit_id,
+            kurikulum=f"{form.tahun_ajaran.data}-{form.kurikulum.data}"
+        )
+
+        if Matakuliah.query.filter_by(kode=matakuliah.kode, is_delete=0).first() is not None:
+            flash(f"Gagal menambahkan data, Matakuliah dengan kode { matakuliah.kode } sudah digunakan", flash_code.DANGER)
+        else:
+            if matakuliah.insert():
+                flash(f"Matakuliah dengan kode { matakuliah.kode } berhasil disimpan", flash_code.SUCCESS)
+                return redirect(url_for('matakuliah.create'))
+            else:
+                flash(f"Gagal menambahkan data, terjadi kesalahan", flash_code.DANGER)
+            
+    return render_template("matakuliah/form.html", form=form, page_title="Buat Matakuliah Baru")
---
+++ app/mod_matakuliah/forms.py
@@ -0,0 +1,48 @@
+"""
+Matakuliah Module's Forms
+"""
+import datetime
+from flask_wtf import FlaskForm
+from wtforms import HiddenField, SelectField, TextAreaField, TextField
+from wtforms.validators import Required
+
+def get_tahun_ajaran():
+    now = datetime.datetime.now()
+    tahun = now.year - 3
+    list_tahun_ajaran = []
+    for i in range(5):
+        list_tahun_ajaran.append(
+            (f"{tahun + i}/{tahun + (i + 1)}", f"{tahun + i}/{tahun + (i + 1)}")
+        )
+    return list_tahun_ajaran
+
+def get_current():
+    now = datetime.datetime.now()
+    tahun = now.year
+    return f"{tahun}/{tahun + 1}"
+
+class MatakuliahForm(FlaskForm):
+    """
+    Form to create or update Matakuliah
+    """
+    matakuliah_id = HiddenField("IdMatakuliah")
+    kode = TextField("Kode", [Required()])
+    nama = TextField("Nama", [Required()])
+    sks = TextField("SKS", [Required()])
+    deskripsi_singkat = TextAreaField("DeskripsiSingkat", [Required()])
+    standar_kompetensi = TextAreaField("StandarKompetensi", [Required()])
+    tahun_ajaran = SelectField(
+        'Role',
+        choices=get_tahun_ajaran(),
+        validators=[Required()],
+        default=get_current()
+    )
+    kurikulum = SelectField(
+        'Role',
+        choices=[
+            ('ganjil', 'Ganjil'),
+            ('genap', 'Genap')
+        ],
+        validators=[Required()],
+        default='ganjil'
+    )
