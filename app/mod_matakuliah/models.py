4cbb121331abafcf0de9ebbbb5b17aacfae7fbf4
Ridho Febriansa <officialridhof@gmail.com>
2020-04-20 17:05:50 +0700

    update max char of deskripsi singkat and standar kompetensi on model and view

--- app/mod_matakuliah/models.py
+++ app/mod_matakuliah/models.py
@@ -13,11 +13,11 @@ class Matakuliah(Base):
     kode = db.Column(db.String(10), nullable=False)
     nama = db.Column(db.String(60), nullable=True)
     sks = db.Column(db.Integer, nullable=True)
-    deskripsi_singkat = db.Column(db.String(200), nullable=True)
-    standar_kompetensi = db.Column(db.String(200), nullable=True)
+    deskripsi_singkat = db.Column(db.String(600), nullable=True)
+    standar_kompetensi = db.Column(db.String(600), nullable=True)
 
-    preprocessed_deskripsi = db.Column(db.String(200), nullable=True)
-    preprocessed_standar = db.Column(db.String(200), nullable=True)
+    preprocessed_deskripsi = db.Column(db.String(600), nullable=True)
+    preprocessed_standar = db.Column(db.String(600), nullable=True)
 
     unit_id = db.Column(db.Integer, nullable=False)
     kurikulum = db.Column(db.String(12), nullable=True)
