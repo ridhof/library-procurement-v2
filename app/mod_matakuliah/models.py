37e2af22a75e976ccf89adae7e3f4d65dcca9dc7
Ridho Febriansa <officialridhof@gmail.com>
2020-04-20 16:42:25 +0700

    add insert, set kurikulum and get kurikulum function

--- app/mod_matakuliah/models.py
+++ app/mod_matakuliah/models.py
@@ -29,10 +29,28 @@ class Matakuliah(Base):
         self.deskripsi_singkat = deskripsi_singkat
         self.standar_kompetensi = standar_kompetensi
         self.unit_id = unit_id
-        self.kurikulum = kurikulum
+        self.set_kurikulum(kurikulum)
 
     def __repr__(self):
         return '<Matakuliah %r>' % (self.kode)
 
+    def set_kurikulum(self, kurikulum):
+        temp_kurikulum = kurikulum.split('-')[1]
+        temp_tahun = kurikulum.split('-')[0].split('/')
+        self.kurikulum = f"{temp_tahun[0][2:]}/{temp_tahun[1][2:]}-{temp_kurikulum}"
+
+    def get_kurikulum(self):
+        temp_kurikulum = self.kurikulum.split('-')[1]
+        temp_tahun = self.kurikulum.split('-')[0].split('/')
+        return f"20{temp_tahun[0]}/20{temp_tahun[1]} {temp_kurikulum.upper()}"
+
     def get_by_unit(unit_id):
         return Matakuliah.query.filter_by(unit_id=unit_id, is_delete=0).all()
+
+    def insert(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return True
+        except:
+            return False
