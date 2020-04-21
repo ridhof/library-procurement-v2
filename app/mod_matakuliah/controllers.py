e84468c040304fdeaf1e6bc7d2351d030de78842
Ridho Febriansa <officialridhof@gmail.com>
2020-04-21 12:51:35 +0700

    add route to display form and update existing Matakuliah data

--- app/mod_matakuliah/controllers.py
+++ app/mod_matakuliah/controllers.py
@@ -56,3 +56,78 @@ def create():
                 flash(f"Gagal menambahkan data, terjadi kesalahan", flash_code.DANGER)
             
     return render_template("matakuliah/form.html", form=form, page_title="Buat Matakuliah Baru")
+
+@MOD_MATAKULIAH.route('/<matakuliah_id>/<matakuliah_kode>/ubah', methods=['GET', 'POST'])
+def update(matakuliah_id, matakuliah_kode):
+    """
+    Return Update Page
+    """
+    user = Staff.is_login()
+    if user is None:
+        return redirect(url_for('auth.login'))
+
+    matakuliah = Matakuliah.get_by_kode(matakuliah_id, matakuliah_kode)
+    if matakuliah is None:
+        flash(f"Terjadi kesalahan, Matakuliah dengan kode { matakuliah_kode } tidak dapat ditemukan", flash_code.WARNING)
+        return redirect(url_for('matakuliah.table'))
+    
+    form = MatakuliahForm(request.form)
+    if form.validate_on_submit():
+        if matakuliah.kode != form.kode.data:
+            if Matakuliah.query.filter_by(kode=form.kode.data, is_delete=0).first() is not None:
+                flash(f"Matakuliah dengan kode { new_kode } sudah digunakan", flash_code.WARNING)
+                form = MatakuliahForm(
+                    data={
+                        'matakuliah_id': matakuliah_id,
+                        'kode': form.kode.data,
+                        'nama': form.nama.data,
+                        'sks': form.sks.data,
+                        'deskripsi_singkat': form.deskripsi_singkat.data,
+                        'standar_kompetensi': form.standar_kompetensi.data,
+                        'tahun_ajaran': form.tahun_ajaran.data,
+                        'kurikulum': form.kurikulum.data
+                    }
+                )
+                return render_template("matakuliah/form.html", form=form, page_title="Ubah Matakuliah")
+        
+        matakuliah_update = Matakuliah.update(
+            matakuliah_id=matakuliah_id,
+            kode=form.kode.data,
+            nama=form.nama.data,
+            sks=form.sks.data,
+            deskripsi_singkat=form.deskripsi_singkat.data,
+            standar_kompetensi=form.standar_kompetensi.data,
+            tahun_ajaran=form.tahun_ajaran.data,
+            kurikulum=form.kurikulum.data
+        )
+        if matakuliah_update:
+            flash(f"Matakuliah dengan Kode { matakuliah_kode } telah berhasil diubah", flash_code.SUCCESS)
+            return redirect(url_for("matakuliah.update", matakuliah_id=matakuliah_id, matakuliah_kode=form.kode.data))
+        
+        flash(f"Terjadi kesalahan pada proses perubahan, data gagal disimpan", flash_code.DANGER)
+        form = MatakuliahForm(
+            data={
+                'matakuliah_id': matakuliah_id,
+                'kode': form.kode.data,
+                'nama': form.nama.data,
+                'sks': form.sks.data,
+                'deskripsi_singkat': form.deskripsi_singkat.data,
+                'standar_kompetensi': form.standar_kompetensi.data,
+                'tahun_ajaran': form.tahun_ajaran.data,
+                'kurikulum': form.kurikulum.data
+            }
+        )
+    else:
+        form = MatakuliahForm(
+            data={
+                'matakuliah_id': matakuliah_id,
+                'kode': matakuliah_kode,
+                'nama': matakuliah.nama,
+                'sks': matakuliah.sks,
+                'deskripsi_singkat': matakuliah.deskripsi_singkat,
+                'standar_kompetensi': matakuliah.standar_kompetensi,
+                'tahun_ajaran': matakuliah.get_kurikulum().split(' ')[0],
+                'kurikulum': matakuliah.get_kurikulum().split(' ')[1].lower()
+            }
+        )
+    return render_template("matakuliah/form.html", form=form, page_title="Ubah Matakuliah")
