7302ac2e835f69fa6e7626625a174399ec97442e
Ridho Febriansa <officialridhof@gmail.com>
2020-04-23 17:27:02 +0700

    add: Pengusulan Code

---
+++ common/pengusulan_code.py
@@ -0,0 +1,11 @@
+"""
+Pengusulan Code module stores constant of Pengusulan Status
+"""
+
+DIUSULKAN = 'diusulkan'
+DISETUJUI_UNIT = 'disetujui_unit'
+DITOLAK_UNIT = 'ditolak_unit'
+DISETUJUI_PUSTAKAWAN = 'disetujui_pustakawan'
+DITOLAK_PUSTAKAWAN = 'ditolak_pustakawan'
+TERPENUHI = 'terpenuhi'
+DIBATALKAN = 'dibatalkan'
