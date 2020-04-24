7c0247f7a6676868fd845494f0dbfdc1812f6890
Ridho Febriansa <officialridhof@gmail.com>
2020-04-24 10:42:10 +0700

    add: Dynamic Matakuliah Multiple Select Field

--- app/mod_pengusulan/forms.py
+++ app/mod_pengusulan/forms.py
@@ -2,9 +2,19 @@
 Pengusulan Module's Forms
 """
 from flask_wtf import FlaskForm
-from wtforms import HiddenField, TextField
+from wtforms import HiddenField, SelectMultipleField, TextField
 from wtforms.validators import Required
 
+from app.mod_matakuliah.models import Matakuliah
+
+def get_matakuliah(unit_id):
+    matakuliahs = Matakuliah.get_by_unit(unit_id)
+    list_matakuliah = []
+    for matakuliah in matakuliahs:
+        list_matakuliah.append(
+            (matakuliah.id, f"{ matakuliah.kode } - { matakuliah.nama }")
+        )
+    return list_matakuliah
 
 class PengusulanBaruForm(FlaskForm):
     """
@@ -14,3 +24,11 @@ class PengusulanBaruForm(FlaskForm):
 
     pengarang = TextField("Pengarang", [Required()])
     judul = TextField("Judul", [Required()])
+    matakuliah = SelectMultipleField(
+        "Matakuliah",
+        choices=[],
+        validators=[Required()]
+    )
+
+    def set_matakuliah(self, unit_id):
+        self.matakuliah.choices = get_matakuliah(unit_id)
