99f379d04ab96b1051bf6230c6ace131015a3db6
Ridho Febriansa <officialridhof@gmail.com>
2020-04-29 16:56:14 +0700

    add: NLP Background Task Function for Preprocessing

---
+++ app/mod_pengusulan/tasks.py
@@ -0,0 +1,17 @@
+"""
+Pengusulan's Tasks
+"""
+import requests
+from common.nlp_preprocess import NLP
+from common import config
+
+
+def preprocess_pengusulan(judul, url):
+    preprocessed_judul = NLP(judul)
+
+    payload = {
+        'preprocessed_judul': preprocessed_judul.preprocessed_text
+    }
+
+    r = requests.post(f"{config.SERVER_URL}{url}", data=payload)
+    print(f"{r.status_code}: {r.text}")
