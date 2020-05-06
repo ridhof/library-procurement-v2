fc428ea1932e3299324e425acf0f60acbb0875d4
Ridho Febriansa <officialridhof@gmail.com>
2020-05-06 15:46:57 +0700

    add: Create Mahasiswa Routes

--- app/mod_mahasiswa/controllers.py
+++ app/mod_mahasiswa/controllers.py
@@ -5,6 +5,7 @@ from flask import Blueprint, flash, redirect, render_template, request, url_for
 
 from app.mod_auth.models import Staff
 from app.mod_mahasiswa.models import Mahasiswa
+from app.mod_mahasiswa.forms import MahasiswaForm
 
 from common import flash_code
 
@@ -22,3 +23,31 @@ def table():
 
     mahasiswas = Mahasiswa.get_by_unit(user.unit_id)
     return render_template("mahasiswa/table.html", mahasiswas=mahasiswas, user=user)
+
+@MOD_MAHASISWA.route('baru', methods=['GET', 'POST'])
+def create():
+    """
+    Return Create Page
+    """
+    user = Staff.is_login()
+    if user is None:
+        return redirect(url_for('auth.login'))
+
+    form = MahasiswaForm(request.form)
+    if form.validate_on_submit():
+        mahasiswa = Mahasiswa(
+            unit_id=user.unit_id,
+            nrp=form.nrp.data,
+            nama=form.nama.data
+        )
+
+        if Mahasiswa.nrp_available(mahasiswa.nrp) is not None:
+            flash(f"Gagal menambahkan data, NRP sudah digunakan", flash_code.DANGER)
+        else:
+            if mahasiswa.insert():
+                flash(f"Mahasiswa berhasil disimpan", flash_code.SUCCESS)
+                return redirect(url_for('mahasiswa.create'))
+            else:
+                flash(f"Gagal menambahkan data, terjadi kesalahan", flash_code.DANGER)
+                
+    return render_template("mahasiswa/form.html", form=form, page_title="Tambah Mahasiswa Baru")
