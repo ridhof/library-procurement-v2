d6395d97ce317c011c9a0e0c6dbf5605b46c4262
Ridho Febriansa <officialridhof@gmail.com>
2020-05-08 17:00:12 +0700

    add: Table Routes

--- app/mod_buku/controllers.py
+++ app/mod_buku/controllers.py
@@ -4,11 +4,23 @@ Buku Module's Controllers
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 
 from app.mod_auth.models import Staff
-from app.mod_buku.models import Dewey
+from app.mod_buku.models import Buku, Dewey
 
 from common import flash_code
 
 MOD_BUKU = Blueprint('buku', __name__, url_prefix='/buku/')
+
+@MOD_BUKU.route('', methods=['GET'])
+def table():
+    """
+    Return Buku Table Page
+    """
+    user = Staff.is_login()
+    if user is None:
+        return redirect(url_for('auth.login'))
+
+    bukus = Buku.get_buku()
+    return render_template("buku/table.html", bukus=bukus, user=user)
 
 @MOD_BUKU.route('dewey/baru', methods=['POST'])
 def store_dewey():
