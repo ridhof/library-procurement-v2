962bd177e2630dccdf6ff88ba8809b0b674d0739
Ridho Febriansa <officialridhof@gmail.com>
2020-05-09 07:26:36 +0700

    add: Create Routes

--- app/mod_buku/controllers.py
+++ app/mod_buku/controllers.py
@@ -5,6 +5,7 @@ from flask import Blueprint, flash, redirect, render_template, request, url_for
 
 from app.mod_auth.models import Staff
 from app.mod_buku.models import Buku, Dewey
+from app.mod_buku.forms import BukuForm
 
 from common import flash_code
 
@@ -21,6 +22,32 @@ def table():
 
     bukus = Buku.get_buku()
     return render_template("buku/table.html", bukus=bukus, user=user)
+
+@MOD_BUKU.route('baru', methods=['GET', 'POST'])
+def create():
+    """
+    Return Create Page
+    """
+    user = Staff.is_login()
+    if user is None:
+        return redirect(url_for('auth.login'))
+    
+    form = BukuForm(request.form)
+    if form.validate_on_submit():
+        buku = Buku(
+            reg_comp=form.reg_comp.data,
+            judul=form.judul.data
+        )
+        if Buku.regcomp_available(buku.reg_comp) is not None:
+            flash(f"Gagal menambahkan data, REG.COMP telah digunakan", flash_code.DANGER)
+        else:
+            if buku.insert():
+                flash(f"Buku berhasil disimpan", flash_code.SUCCESS)
+                return redirect(url_for('buku.create'))
+            else:
+                flash(f"Gagal menambahkan data, terjadi kesalahan", flash_code.DANGER)
+    
+    return render_template("buku/form.html", form=form, page_title="Tambah Buku Buru")
 
 @MOD_BUKU.route('dewey/baru', methods=['POST'])
 def store_dewey():
