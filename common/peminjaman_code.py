764c5c449124bc2feceaaab090137a461ba653d6
Ridho Febriansa <officialridhof@gmail.com>
2020-05-13 16:22:21 +0700

    add: Function to return status as tuple on views

--- common/peminjaman_code.py
+++ common/peminjaman_code.py
@@ -1,3 +1,12 @@
 PINJAM = 'pinjam'
 KEMBALI = 'kembali'
 PERPANJANG = 'perpanjang'
+
+
+def get_status():
+    list_status = [
+        (PINJAM, PINJAM.capitalize()),
+        (KEMBALI, KEMBALI.capitalize()),
+        (PERPANJANG, PERPANJANG.capitalize())
+    ]
+    return list_status
