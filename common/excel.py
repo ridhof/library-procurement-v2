2af29f16036921f56ba3d54455a1d4b79d6a2a40
Ridho Febriansa <officialridhof@gmail.com>
2020-05-17 20:02:38 +0700

    add: store and send function to send retrieved data to endpoint

--- common/excel.py
+++ common/excel.py
@@ -1,4 +1,6 @@
+import requests
 import xlrd
+from common import config
 
 
 # Example to run:
@@ -67,3 +69,17 @@ class Excel():
             if i != 0:
                 data.append(file_sheet.row_values(i))
         return data
+
+    def store(self, url):
+        all_data = self.get_all_data()
+        for i in range(len(all_data)):
+            Excel.send(self.column_names, all_data[i], url)
+
+    def send(names, data, url):
+        payload = {}
+        for i in range(len(names)):
+            name = names[i].lower().replace(' ', '_')
+            payload[name] = data[i] 
+        print(payload)
+        r = requests.post(f"{config.SERVER_URL}{url}", data=payload)
+        print(f"{r.status_code}: {r.text}")
