233276176b0e9b34b233f46251e04418f32c7eb8
Ridho Febriansa <officialridhof@gmail.com>
2020-06-13 17:25:41 +0700

    add: unit_id and perpus_role on StaffForm

--- app/mod_staff/forms.py
+++ app/mod_staff/forms.py
@@ -5,6 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import HiddenField, PasswordField, SelectField, TextField
 
 from wtforms.validators import Required
+from common import perpus_code
 
 
 class StaffForm(FlaskForm):
@@ -14,6 +15,7 @@ class StaffForm(FlaskForm):
     staff_id = HiddenField("IdStaff")
     npk = TextField('NPK', [Required()])
     nama = TextField('Nama', [Required()])
+    unit_id = TextField('UnitId', default='')
     role = SelectField(
         'Role',
         choices=[
@@ -23,6 +25,16 @@ class StaffForm(FlaskForm):
         ],
         validators=[Required()],
         default='staff'
+    )
+    perpus_role = SelectField(
+        'PerpusRole',
+        choices=[
+            (perpus_code.ANGGOTA, 'Pemustaka'),
+            (perpus_code.PEGAWAI, 'Pegawai'),
+            (perpus_code.KEPALA_BAGIAN, 'Kepala Bagian'),
+            (perpus_code.DIREKTUR, 'Direktur')
+        ],
+        default=perpus_code.ANGGOTA
     )
 
 
