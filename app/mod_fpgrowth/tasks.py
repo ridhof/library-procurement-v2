51853d72b4edf47132c0ea1f5d247c34842a61e7
Ridho Febriansa <officialridhof@gmail.com>
2020-08-17 22:15:17 +0700

    add: Console Log on FPGrowth

--- app/mod_fpgrowth/tasks.py
+++ app/mod_fpgrowth/tasks.py
@@ -7,11 +7,14 @@ from common import config
 
 
 def do_fpgrowth(peminjamans, url):
+    print(f"Running FP Growth Process using {len(peminjamans)}")
     transactions = []
 
     tanggal_looping = peminjamans[0]['tanggal_pinjam']
     pemustaka_looping = peminjamans[0]['pemustaka']
     pemustaka_transaction = []
+
+    print("Looping on Peminjaman")
     for peminjaman in peminjamans:
         pemustaka = peminjaman['pemustaka']
         tanggal_pinjam = peminjaman['tanggal_pinjam']
@@ -27,10 +30,15 @@ def do_fpgrowth(peminjamans, url):
             pemustaka_looping = pemustaka
             pemustaka_transaction = [peminjaman_clustering_id]
     transactions.append(pemustaka_transaction)
-    
+    print(f"{len(transactions)} transactions achieved")
+
+    print("Calculating FPGrowth")
     fpgrowth = FPGrowth(transactions)
+    print("FPGrowth Result Retrieved")
+
+    print("Looping FPGrowth Result")
     for rule in fpgrowth.association_rules:
-        # print(rule)
+        print(f"Sending Looped Rule to Endpoint, {rule}")
         payload = rule
         r = requests.post(f"{config.SERVER_URL}{url}", data=payload)
         print(f"{r.status_code}: {r.text}")
