f9580a34d185212fc8fb5143e9e088fac6f68cfb
Ridho Febriansa <officialridhof@gmail.com>
2020-08-17 22:07:57 +0700

    fix: FPGrowth Get by Period and Cluster Dict Methods

--- app/mod_fpgrowth/models.py
+++ app/mod_fpgrowth/models.py
@@ -41,6 +41,7 @@ class FrequentPatternGrowth(Base):
                     clustering_id)
                 if clustering_details is not None:
                     peminjaman_clusterings = []
+                    peminjaman_clustering_labels = []
                     for detail in clustering_details:
                         peminjaman_clustering = PeminjamanClustering.get_by_detail(
                             detail.id)
@@ -51,19 +52,33 @@ class FrequentPatternGrowth(Base):
                         fpgrowth = FrequentPatternGrowth.query.filter_by(
                             main_itemset=peminjaman.id, is_delete=0).all()
                         fpgrowths.extend(fpgrowth)
-                    return fpgrowths
-
+                    
+                    itemsets = []
+                    fpgrowths_result = []
+                    for fpgrowth in fpgrowths:
+                        main = fpgrowth.get_cluster_dict(fpgrowth.main_itemset)
+                        correlate = fpgrowth.get_cluster_dict(fpgrowth.correlated_itemset)
+                        itemset = f"{main} {correlate}"
+                        itemset_reverse = f"{correlate} {main}"
+                        if itemset not in itemsets and main != correlate and itemset_reverse not in itemsets:
+                            itemsets.append(itemset)
+                            fpgrowths_result.append(fpgrowth)
+                    [print(f"Itemset ({associate.get_cluster_dict(associate.main_itemset)}, {associate.get_cluster_dict(associate.correlated_itemset)}). Support {associate.support_value}. Confidence {associate.confidence_value}. Lift {associate.lift_value}") for associate in fpgrowths_result]
+                    return fpgrowths_result
             return []
         except:
             return []
 
-    def get_cluster_dict(self, peminjaman_clustering_id):
+    def get_cluster_dict(self, peminjaman_clustering_id, is_display=False):
         result = peminjaman_clustering_id
         peminjaman_clustering = PeminjamanClustering.find(peminjaman_clustering_id)
         if peminjaman_clustering is not None:
             clustering_detail = ClusteringDetail.find_by_id(peminjaman_clustering.clustering_detail_id)
             if clustering_detail is not None:
                 result = clustering_detail.cluster_dict
+                if is_display:
+                    array_dict = result.split('-')
+                    result = f"{array_dict[1]} {array_dict[0]}".title()
         return result
 
     def insert(self):
